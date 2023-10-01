4cfd9da504092e355437dd41c9b611d7e6f48117
ABDUSH <abdush@ABDUSHs-MacBook-Pro.local>
2023-10-01 18:37:14 +0530

    first commit

--- main.go
+++ main.go
@@ -11,6 +11,70 @@ import (
 	"github.com/gocarina/gocsv"
 )
 
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	humans := readHumans("assets/values.csv")
+
+	for _, v := range humans {
+		if v.Age < 40 {
+			v.Survived = 1
+		}
+	}
+
+	perc := getSurvPercentage(humans)
+
+	fmt.Println(fmt.Sprintf("Correct : %.2f", perc))
+}
+
+func readHumans(name string) []*Human {
+	bs, err := os.ReadFile(name)
+	checkErr(err)
+	var hs []*Human
+	err = gocsv.UnmarshalBytes(bs, &hs)
+	checkErr(err)
+	return hs
+}
+
+func getSurvPercentage(humans []*Human) float64 {
+	surv := readHumans("assets/surv.csv")
+
+	correct := 0.0
+
+	for _, c := range surv {
+		for _, n := range humans {
+			if c.Id != n.Id {
+				continue
+			}
+			if c.Survived == n.Survived {
+				correct++
+				break
+			}
+		}
+	}
+
+	return (correct / float64(len(surv))) * 100
+}
+
+type Human struct {
+	Id          int     `csv:"id"`
+	Survived    int     `csv:"Survived,omitempty"`
+	PassengerId int     `csv:"PassengerId"`
+	Pclass      int     `csv:"Pclass"`
+	Name        string  `csv:"Name"`
+	Sex         string  `csv:"Sex"`
+	Age         int     `csv:"Age"`
+	SibSp       int     `csv:"SibSp"`
+	Parch       int     `csv:"Parch"`
+	Ticket      string  `csv:"Ticket"`
+	Fare        float64 `csv:"Fare"`
+	Embarked    string  `csv:"Embarked"`
+}
+
 func AddId() {
 	f, err := os.Open("assets/survived.csv")
 	checkErr(err)
@@ -30,38 +94,4 @@ func AddId() {
 	}
 	wr.Flush()
 	ioutil.WriteFile("assets/surv.csv", bts.Bytes(), fs.ModePerm)
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func main() {
-	AddId()
-}
-
-func readHumans(name string) []*Human {
-	bs, err := os.ReadFile(name)
-	checkErr(err)
-	var hs []*Human
-	err = gocsv.UnmarshalBytes(bs, &hs)
-	checkErr(err)
-	return hs
-}
-
-type Human struct {
-	Id          int     `csv:"id"`
-	Survived    int     `csv:"Survived,omitempty"`
-	PassengerId int     `csv:"PassengerId"`
-	Pclass      int     `csv:"Pclass"`
-	Name        string  `csv:"Name"`
-	Sex         string  `csv:"Sex"`
-	Age         int     `csv:"Age"`
-	SibSp       int     `csv:"SibSp"`
-	Parch       int     `csv:"Parch"`
-	Ticket      string  `csv:"Ticket"`
-	Fare        float64 `csv:"Fare"`
-	Embarked    string  `csv:"Embarked"`
 }
